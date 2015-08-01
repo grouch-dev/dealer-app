11833f49b281af7aaf5511d68940f47fbe002a0d
Scott Woolcox <scott.woolcox@autodata.net>
2015-08-01 16:29:13 -0400

    First commit

---
+++ dealer-etl/dealer-etl/src/main/java/woolcox/dealer/etl/App.java
@@ -0,0 +1,13 @@
+package woolcox.dealer.etl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
